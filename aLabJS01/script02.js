4c1a4222dbf1eafdc73121b08b39625e395eadfc
Ari Jesus <ari.vj.pacheco@ubi.pt>
2025-03-17 10:00:43 +0000

    gugu

--- aLabJS01/script02.js
+++ aLabJS01/script02.js
@@ -2,5 +2,16 @@ const botao  = document.getElementById("botao");
 
 botao.addEventListener("click", function(){
     const h1 = document.querySelector("h1");
-    h1.textContent = "Botão Clicado!";
+    h1.textContent = "Olá, mundo!";
+    h1.style.backgroundColor = "red";
+});
+
+
+const campoTexto = document.getElementById("campoTexto");
+
+campoTexto.addEventListener("keydown", function(enter) {
+    if (enter.key === "Enter") {  
+        console.log("Valor do campo:", campoTexto.value);
+        campoTexto.value = "";
+    }
 });
