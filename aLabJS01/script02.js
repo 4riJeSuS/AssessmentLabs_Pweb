e5c24452cebd621fa0e701ae66d6c994ab396b74
Tomas-Silva2005 <155886494+Tomas-Silva2005@users.noreply.github.com>
2025-03-17 10:08:19 +0000

    fasfsdfsdfs

--- aLabJS01/script02.js
+++ aLabJS01/script02.js
@@ -15,3 +15,13 @@ campoTexto.addEventListener("keydown", function(enter) {
         campoTexto.value = "";
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lista = document.getElementById("lista");
+
+    lista.addEventListener("click", function (event) {
+        if (event.target.tagName === "LI") {
+            event.target.remove(); 
+        }
+    });
+});
