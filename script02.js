37e3b128bfb8af08d136c5fb629c44626f2bf618
Ari Jesus <ari.vj.pacheco@ubi.pt>
2025-03-17 10:12:52 +0000

    heheh

--- script02.js
+++
@@ -1,6 +0,0 @@
-const botao  = document.getElementById("botao");
-
-botao.addEventListener("click", function(){
-    const h1 = document.querySelector("h1");
-    h1.textContent = "Botão Clicado!";
-});
