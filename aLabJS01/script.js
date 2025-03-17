289a44c8729c2094110f0e2f0aae8b5a79c76ade
Ari Jesus <ari.vj.pacheco@ubi.pt>
2025-03-17 09:26:42 +0000

    ifuavwuiefauilwbfcyuilawgeyuilrfvaeyuk

---
+++ aLabJS01/script.js
@@ -0,0 +1,49 @@
+//**var **
+
+var nome = "João";
+console.log(nome);
+
+nome = "Maria";
+console.log(nome);
+
+// **let**
+
+let idade = 20;
+console.log(idade);
+
+idade = 21;
+console.log(idade);
+
+// **const**
+const PI = 3.14;
+console.log(PI); 
+
+// **Tipos de Dados**
+
+// String
+let frase = "Olá, mundo!";
+console.log(typeof frase);
+
+// Number 
+let numero = 10;
+console.log(typeof numero);
+
+// Boolean
+let verdadeiro = true;
+console.log(typeof verdadeiro);
+
+//Array
+let lista = ["banana", "maça", "laranja"];
+console.log(typeof lista);
+
+// Object 
+let pessoa = { name: "João", idade: 20};
+console.log(typeof pessoa);
+
+// null
+let nulo = null;
+console.log(typeof nulo);
+
+// undefined
+let indefinido;
+console.log(typeof indefinido);
